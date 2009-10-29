b0ea56f95ca7c8bb49f88663bc085396dec60106
Brian Cardarella <cardarellab@dnc.org>
2009-10-29 12:04:00 -0400

    Added background images   - Header now a separate block   - Using Cufon to style the Header App Title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def flash_helper
+    returning("") do |helpers|
+      flash.each do |key, message|
+        helpers << content_tag(:div, message, :class => "flash-#{key}")
+      end
+    end
+  end
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
 
-  APP_NAME = "some_name"
+  APP_NAME  = "some_name"
   APP_TITLE = "Some Title"
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
