4072d98bcb9ccb76df90d59b9453605a6825564e
Nathan Woodhull <woodhull@Nathan-Woodhulls-MacBook-Pro.local>
2009-11-04 15:49:03 -0500

    some basic partner validations, api key creation

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20091104170848) do
+
+  create_table "partners", :force => true do |t|
+    t.string   "name"
+    t.string   "organization"
+    t.string   "phone_number"
+    t.string   "email"
+    t.string   "api_key"
+    t.integer  "max_requests"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
--- test/unit/partner_test.rb
+++ test/unit/partner_test.rb
@@ -1,8 +1,49 @@
 require 'test_helper'
 
 class PartnerTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context "a partner object" do
+    setup { @partner = Partner.new }
+    subject { @partner }
+    should_validate_presence_of :name, :organization, :phone_number, :email
+
+    should "reject bad emails" do
+      bads = ["bad bad", "bad_email_something"]
+      goods = ["good@google.com", "good+email@potato.com"]
+      bads.each do |email|
+        @partner.email = email
+        @partner.save
+        assert @partner.errors.on(:email)
+      end
+      goods.each do |email|
+        @partner.email = email
+        @partner.save
+        assert !@partner.errors.on(:email)
+      end
+    end
+
+    context "phone Number" do
+      context "Normalization" do
+        should "remove all non numerics from the phone number" do
+          @partner.phone_number = "123-456-7890"
+          assert_equal "1234567890", @partner.attributes["phone_number"]
+          @partner.phone_number = "123.456.7890"
+          assert_equal "1234567890", @partner.attributes["phone_number"]
+          @partner.phone_number = "(123) 456-7890"
+          assert_equal "1234567890", @partner.attributes["phone_number"]
+        end
+
+        should "reformat the phone_number into (xxx) xxx-xxxx" do
+          @partner.phone_number = 1234567890
+          assert_equal "(123) 456-7890", @partner.phone_number
+        end
+      end
+    end
+
+    context "api key" do
+      should "create api key before validations" do
+        @partner.valid?
+        assert @partner.api_key.present?
+      end
+    end
   end
 end
