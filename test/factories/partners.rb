53b82e5d1618adff2ec8e4010bce8851826610a2
Nathan Woodhull <woodhull@Nathan-Woodhulls-MacBook-Pro.local>
2009-11-04 16:12:00 -0500

    partners controller and test

--- test/factories/partners.rb
+++ test/factories/partners.rb
@@ -2,6 +2,8 @@ Factory.define :partner do |p|
   p.name { "George Washington"}
   p.organization { "Cherry Tree Cutters"}
   p.email { "george@ctc.com"}
+  p.phone_number { "1112223333"}
+
   p.api_key        { Factory.next(:api_key) }
 end
 
