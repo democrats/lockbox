a46835f26f22ab05aef08352ba52eae99fd6878d
Chris Gill <gill@gillc17.local>
2009-11-18 10:25:42 -0500

    adding create action back to partners admin

--- app/controllers/admin/partners_controller.rb
+++ app/controllers/admin/partners_controller.rb
@@ -2,12 +2,12 @@ class Admin::PartnersController < AdminController
 
   active_scaffold :partner do |config|
     config.label = "API Partners"
-    config.actions.exclude :create
     update.columns.exclude [:password, :password_confirmation]
     list.columns = [:id, :email, :name, :organization, :phone_number, :api_key, :max_requests]
+    create.columns = [:email, :name, :organization, :phone_number, :max_requests, :password, :password_confirmation]
     list.sorting = {:id => 'DESC'}
     update.columns.exclude :api_key
-    search.columns = [:email, :name, :organization]
+    search.columns = [:email, :name, :organization, :api_key]
     columns[:max_requests].label = "Requests per Hour"
   end
 
