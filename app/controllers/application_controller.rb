0f9c50e28d09afc69f90dbe27f370d51dcf51b22
Chris Gill <gill@gillimacs.home>
2010-05-03 22:12:51 -0400

    removing unused code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,19 +15,4 @@ class ApplicationController < ActionController::Base
   # filter_parameter_logging :password
   
   
-  def render_jsonp(json, options={})
-    callback, variable = params[:callback], params[:variable]
-    response = begin
-      if callback && variable
-        "var #{variable} = #{json};\n#{callback}(#{variable});"
-      elsif variable
-        "var #{variable} = #{json};"
-      elsif callback
-        "#{callback}(#{json});"
-      else
-        json
-      end
-    end
-    render({:text => response}.merge(options))
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,20 +17,4 @@ module ApplicationHelper
     end
   end
 
-  def render_jsonp(json, options={})
-    callback, variable = params[:callback], params[:variable]
-    response = begin
-      if callback && variable
-        "var #{variable} = #{json};\n#{callback}(#{variable});"
-      elsif variable
-        "var #{variable} = #{json};"
-      elsif callback
-        "#{callback}(#{json});"
-      else
-        json
-      end
-    end
-    render({:text => response}.merge(options))
-  end
-
 end
