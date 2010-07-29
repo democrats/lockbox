d37b9968719ec47120100c0faecc8985e8654f1b
Nathan Woodhull <woodhulln@dnc.org>
2010-07-29 13:54:48 -0400

    exercise all of the partner controller code

--- spec/controllers/admin/partners_controller_spec.rb
+++ spec/controllers/admin/partners_controller_spec.rb
@@ -32,11 +32,28 @@ describe Admin::PartnersController do
       assert_raise(ActiveRecord::RecordNotFound) {  Partner.find(@partner.id) }
     end
 
+    it "should not be able to destroy a partner" do
+      Partner.any_instance.stubs(:destroy).returns(false)
+      assert Partner.find(@partner.id)
+      delete :destroy, :id => @partner.id
+      assert_redirected_to admin_partners_path
+      assert Partner.find(@partner.id)
+    end
+
     it "should be able to update a partner" do
       put :update, :id => @partner.id, :partner => Factory.attributes_for(:partner, :name => 'crazyname', :max_requests => 1234, 
         :password => nil, :password_confirmation => nil)
       assert_equal("crazyname", @partner.reload.name)
       assert_equal(1234, @partner.max_requests)
+    end
+
+    it "should not be able to update an invalud partner" do
+      partner_email = @partner.email
+      partner_max_requests = @partner.max_requests
+      put :update, :id => @partner.id, :partner => Factory.attributes_for(:partner, :email => 'invalidemail', :max_requests => 1234,
+        :password => nil, :password_confirmation => nil)
+      assert_equal(partner_email, @partner.reload.email)
+      assert_equal(partner_max_requests, @partner.max_requests)
     end
     
   end
